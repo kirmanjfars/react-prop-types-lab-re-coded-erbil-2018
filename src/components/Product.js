0242b910b092ff58aef5e8bc563997c1ca03f099
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-09-04 17:21:51 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,12 +8,7 @@ class Product extends React.Component {
   render() {
     return (
       <div className="order">
-        <ul>
-          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
-          <li>{this.props.size}</li>
-          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
-          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
-        </ul>
+        
       </div>
     );
   }
